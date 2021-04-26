23ae9d06fbe9b2cdb11aa5a9df654e956bc9feab
draab <djamel.r.75@gmail.com>
2021-04-26 20:24:16 +0200

    Add feature get current price of cryptocurrency Add animation ploting current price of cryptocurrency

--- src/data/historicalPriceData.py
+++ src/data/historicalPriceData.py
@@ -2,9 +2,12 @@ import pandas as pd
 import requests
 from datetime import datetime
 from utils import BASE_URL, data_to_dataframe, plot_data
+import cryptocompare
 
-def get_hist_data(from_sym='BTC', to_sym='USD', timeframe = 'day', limit=2000, aggregation=1, exchange='', toTs=None):
-    """ Download the JSON via Cryptocompare API
+# To delete because cryptocompare.get_historical_price* do the samme job 
+def get_histo_data(from_sym='BTC', to_sym='USD', timeframe = 'day', limit=2000, aggregation=1, exchange='', toTs=None):
+    """ Download the historical data via Cryptocompare API
+        using request package 
     """
 
     url = BASE_URL + '/v2/histo'
@@ -31,3 +34,19 @@ def get_hist_data(from_sym='BTC', to_sym='USD', timeframe = 'day', limit=2000, a
     data = response.json()['Data']['Data'] 
     
     return data
+
+
+if __name__ == "__main__" : 
+    cryptocurrency = 'BTC'
+    target_currency = 'USD'
+
+    # Test get_hist_data 
+    #data = get_histo_data(cryptocurrency, target_currency, 'minute', 1000)
+    data = cryptocompare.get_historical_price_minute('BTC', 'EUR', limit=24, exchange='CCCAGG', toTs=datetime.now())
+    df = data_to_dataframe(data)
+    df.to_parquet('df-minutes.parquet.gzip',
+              compression='gzip')  # Save data in local file system
+    df = pd.read_parquet('df.parquet.gzip')
+    print(df.tail())
+    plot_data(df, cryptocurrency, target_currency)    
+
--- src/data/utils.py
+++ src/data/utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+import cryptocompare
 
 BASE_URL = 'https://min-api.cryptocompare.com/data'
 
@@ -13,10 +14,8 @@ def data_to_dataframe(data):
     # time is stored as an epoch, we need normal dates
     df['time'] = pd.to_datetime(df['time'], unit='s')
     df.set_index('time', inplace=True)
-    print(df.tail())
-    
-    return df
 
+    return df
 
 def plot_data(df, cryptocurrency, target_currency):
     # got his warning because combining matplotlib 
@@ -30,9 +29,32 @@ def plot_data(df, cryptocurrency, target_currency):
     register_matplotlib_converters()
     
     plt.figure(figsize=(15,5))
+    plt.cla()
+    plt.gcf().canvas.set_window_title('Plot Cryptocurrency')
     plt.title('{} / {} price data'.format(cryptocurrency, target_currency))
     plt.plot(df.index, df.close)
     plt.legend()
     plt.show()
     
     return None
+
+def get_crypto_infos(path_target):
+    """ Download the information coin list via Cryptocompare API 
+        then transform JSON paylod into Pandas dataframe and save
+        in parquet format.
+    """
+    #data from json is one dictionarie by coin {{coin:{infos}},...}
+    coins_infos =  cryptocompare.get_coin_list()
+
+    # Transpose the dataframe to have coin symbol in index and information in columns
+    df_coins_infos = pd.DataFrame.from_dict(coins_infos).T
+
+    df_coins_infos.to_parquet(path_target,
+                            partition_cols = 'Symbol',
+                            compression='snappy')  
+
+
+if __name__ == "__main__" : 
+
+    # Test save crypto informations in local file system
+    get_crypto_infos('./coins-infos.parquet') 
