47577de1bef0168a411ed555daac2fb6e2415339
AnisBarr <56957204+AnisBarr@users.noreply.github.com>
2021-06-29 20:51:24 +0200

    Update venv-entrypoint.sh

--- src/scraper_price/app/venv-entrypoint.sh
+++ src/scraper_price/app/venv-entrypoint.sh
@@ -3,6 +3,6 @@ cd /
 
 export project_id=pa5-crypto-advice2
 export topic_id=trades-flow
-export GOOGLE_APPLICATION_CREDENTIALS=/conf/key-file.json
+export GOOGLE_APPLICATION_CREDENTIALS=/conf/.key-file.json
 
 python /app/price_getter.py
