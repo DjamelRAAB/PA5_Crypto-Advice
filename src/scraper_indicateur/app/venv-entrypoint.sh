ef5e2bbc45422912aee5dfa9c36dfe5806fb2339
AnisBarr <56957204+AnisBarr@users.noreply.github.com>
2021-06-29 20:49:33 +0200

    Update venv-entrypoint.sh

--- src/scraper_indicateur/app/venv-entrypoint.sh
+++ src/scraper_indicateur/app/venv-entrypoint.sh
@@ -3,6 +3,6 @@ cd /
 
 export project_id=pa5-crypto-advice2
 export topic_id=metrics-flow
-export GOOGLE_APPLICATION_CREDENTIALS=/conf/key-file.json
+export GOOGLE_APPLICATION_CREDENTIALS=/conf/.key-file.json
 export API_KEY=1sDGH1SMX74Z1deZFm32inPjYId
 /usr/bin/python3.8 /app/indicateur_data.py
